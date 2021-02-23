b42f3064da68f3234e5620b94e4f8643691a31e5
John <78956981+johnfranklinrickard@users.noreply.github.com>
2021-02-23 01:38:07 +0100

    Add initial solution files
    Create Solution for .NET 5
    Add SFML package
    Add editorconfig
    Initial program creates SFML window with a green circle as a test

---
+++ 2DGame/Program.cs
@@ -0,0 +1,41 @@
+﻿using SFML.Audio;
+using SFML.Graphics;
+using SFML.Window;
+
+namespace Game
+{
+    internal static class Program
+    {
+        public static void Main()
+        {
+            var mode = new VideoMode(1280, 960);
+            var window = new RenderWindow(mode, "SFML works!");
+            window.KeyPressed += OnKeyPress;
+
+            var circle = new CircleShape(100f)
+            {
+                FillColor = Color.Green
+            };
+
+            var music = new Music(@"..\..\..\music\First Quarter.ogg");
+            music.Play();
+
+            while (window.IsOpen)
+            {
+                window.DispatchEvents();
+                window.Clear(Color.White);
+                window.Draw(circle);
+                window.Display();
+            }
+        }
+
+        private static void OnKeyPress(object sender, KeyEventArgs e)
+        {
+            if (e.Code == Keyboard.Key.Escape)
+            {
+                var window = (Window)sender;
+                window.Close();
+            }
+        }
+    }
+}
