31422955334bcd603d0ec9b98488a281cb59413d
John <78956981+johnfranklinrickard@users.noreply.github.com>
2021-02-23 16:17:20 +0100

    Add timers with console output and fix framerate to 60 frames per second

--- 2DGame/Program.cs
+++ 2DGame/Program.cs
@@ -1,6 +1,8 @@
 ﻿using SFML.Audio;
 using SFML.Graphics;
+using SFML.System;
 using SFML.Window;
+using System;
 
 namespace Game
 {
@@ -9,8 +11,10 @@ namespace Game
         public static void Main()
         {
             var mode = new VideoMode(1280, 960);
-            var window = new RenderWindow(mode, "SFML works!");
+            var window = new RenderWindow(mode, "SFML works!", Styles.Close);
             window.KeyPressed += OnKeyPress;
+            window.Closed += (_, _) => window.Close();
+            window.SetFramerateLimit(60);
 
             var circle = new CircleShape(100f)
             {
@@ -20,12 +24,27 @@ namespace Game
             var music = new Music(@"..\..\..\music\First Quarter.ogg");
             music.Play();
 
+            Console.WriteLine();
+
+            var clock = new Clock();
+
             while (window.IsOpen)
             {
                 window.DispatchEvents();
+                var dispatchTime = clock.ElapsedTime;
+
                 window.Clear(Color.White);
                 window.Draw(circle);
+                var drawTime = clock.ElapsedTime - dispatchTime;
+
                 window.Display();
+
+
+                var end = clock.ElapsedTime;
+                Console.WriteLine($"Dispatch: {dispatchTime.AsMicroseconds()}us  " +
+                    $"Draw: {drawTime.AsMicroseconds()}us  Total: {end.AsMicroseconds()}us");
+
+                clock.Restart();
             }
         }
 
